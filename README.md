ef42e3cd20d2d512dabf7a8b300b402ff94a3075
Surudoiryu <nigelbloemendal@gmail.com>
2017-03-15 11:17:54 +0100

    Init commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# simple-rulesbased-chatbot
+
+A simple rules based chatbot, that can tell you the weather and simple things like hello, bye.
+
+# How to run
+
+```
+npm install
+
+./ngrok http 3000
+```
+
+Copy the Ngrok link and paste this in the App on Facebook as CallbackURL
+
+# Example commands
+
+* Hi
+* Bye
+* What is the weather like in Zeist?
+* Will it snow tomorrow in Zeist?
---
+++ server.js
@@ -0,0 +1,80 @@
+'use strict';
+
+// Create an API server
+const restify = require('restify');
+const server = restify.createServer({
+	name: 'VanillaMessenger'
+});
+const PORT = process.env.PORT || 3000;
+
+// 
+server.use( restify.jsonp() );
+server.use( restify.bodyParser() );
+
+// Tokens
+const config = require('./config');
+
+// FBeamer
+const FBeamer = require('./fbeamer');
+const f = new FBeamer(config);
+
+// VanillaMessenger
+const matcher = require('./matcher');
+const weather = require('./weather');
+const {currentWeather, forecastWeather} = require('./parser');
+
+// Register the webhooks
+server.get('/', (req, res, next) => {
+	f.registerHook(req, res);
+	return next();
+});
+
+// Receive all incoming messages
+server.post('/', (req, res, next) => {
+	f.incoming(req, res, msg => {
+		// Process messages
+		//f.txt(msg.sender, `Hey, you just said ${msg.message.text}`);
+		//f.img(msg.sender, `http://www.stickees.com/files/food/sweet/3543-icecream-cone-sticker.png`)
+		if(msg.message.text) {
+			// If the received message is a text message
+			matcher(msg.message.text, data => {
+				switch(data.intent) {
+					case 'Hello':
+						f.txt(msg.sender, `${data.entities.greeting} to you too!`);
+						break;
+					case 'CurrentWeather':
+						weather(data.entities.city, 'current')
+							.then(response => {
+								let parseResult = currentWeather(response);
+								f.txt(msg.sender, parseResult);
+							})
+							.catch(error => {
+								console.log("There seems to be a problem connecting to the weather service "+error);
+								f.txt(msg.sender, "Hmm, something is not right with my brains. Please try again later.")
+							});
+						break;
+					case 'WeatherForecast': 
+						weather(data.entities.city)
+							.then(response => {
+								let parseResult = forecastWeather(response, data.entities);
+								f.txt(msg.sender, parseResult);
+							})
+							.catch(error => {
+								console.log("There seems to be a problem connecting to the weather service "+error);
+								f.txt(msg.sender, "Hmm, something is not right with my brains. Please try again later.")
+							});
+						break;
+					default: {
+						f.txt(msg.sender, "Gosh! I don't know George :( what do you mean?")
+					}
+				}
+			});
+		}
+	});
+	return next();
+});
+
+// Auto subscribe the bot
+f.subscribe();
+
+server.listen(PORT, () => console.log(`SPH Seller Bot running on port ${PORT}`));
